aecf4721ddb45f20e5e2d160721d8f3bd015eb73
rokugatsua <rokugatsua@outlook.co.id>
2020-08-02 12:30:18 +0700

    add min max offset pipe

--- Assets/Scripts/PipeSpawner.cs
+++ Assets/Scripts/PipeSpawner.cs
@@ -9,6 +9,7 @@ public class PipeSpawner : MonoBehaviour
     [SerializeField] private Pipe pipeUp, pipeDown;
     [SerializeField] private float spawnInterval = 1;
     [SerializeField] private float holeSize = 1f;
+    [SerializeField] private float maxMinOffset = 1;
 
     private Coroutine CR_Spawn;
     // Start is called before the first frame update
@@ -44,6 +45,11 @@ public class PipeSpawner : MonoBehaviour
         newPipeUp.transform.position += Vector3.up * (holeSize / 2);
         newPipeDown.transform.position += Vector3.down * (holeSize / 2);
 
+        // Spawnpipe offset
+        float y = maxMinOffset * Mathf.Sin(Time.time);
+        newPipeUp.transform.position += Vector3.up * y;
+        newPipeDown.transform.position += Vector3.up * y;
+        
     }
 
     IEnumerator IeSpawn()
