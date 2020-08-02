9ab4ae1a381fbdcfc5f65d5416ccc179330c95b8
rokugatsua <rokugatsua@outlook.co.id>
2020-08-02 11:42:43 +0700

    add Ground spawn

---
+++ Assets/Scripts/GroundSpawner.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+[RequireComponent(typeof(Rigidbody2D))]
+public class GroundSpawner : MonoBehaviour
+{
+    // Global Variables
+    [SerializeField]
+    private Ground groundRef;
+    private Ground prevGround;
+
+    private void SpawnGround()
+    {
+        if (prevGround != null) {
+            Ground newGround = Instantiate(groundRef);
+            newGround.gameObject.SetActive(true);
+            prevGround.SetNextGround(newGround.gameObject);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other) {
+        Ground ground = other.GetComponent<Ground>();
+
+        if (ground) {
+            prevGround = ground;
+            SpawnGround();
+        }
+        
+    }
+}
