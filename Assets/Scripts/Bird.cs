ebbab2043303cbdff5c4a5398177195caa84d4c3
rokugatsua <rokugatsua@outlook.co.id>
2020-08-02 10:49:52 +0700

    add jump and onDead to Bird

---
+++ Assets/Scripts/Bird.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Bird : MonoBehaviour
+{
+    // Global Variables
+    [SerializeField] private float upForce = 100;
+    [SerializeField] private bool isDead;
+    [SerializeField] private UnityEvent OnJump, OnDead;
+    private Rigidbody2D rigidbody2d;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2d = GetComponent<Rigidbody2D>();        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isDead && Input.GetMouseButtonDown(0)) {
+            Jump();
+        }
+        
+    }
+
+    public bool IsDead()
+    {
+        return isDead;
+    }
+
+    public void Dead()
+    {
+        if (!isDead && OnDead != null) {
+            OnDead.Invoke();
+        }
+
+        isDead = true;
+    }
+
+    void Jump()
+    {
+        if (rigidbody2d) {
+            rigidbody2d.velocity = Vector2.zero;
+            rigidbody2d.AddForce(new Vector2(0, upForce));
+        }
+
+        if (OnJump != null) {
+            OnJump.Invoke();
+        }
+    }
+}
